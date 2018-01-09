b3d1f75fc7125a1edc502c25bfb23d5247c8f6e3
wpancini <wpancini@gmail.com>
2018-01-08 23:34:07 -0200

    alteração da string de conexão

--- src/br/com/cwmconsultoria/bd/DBConfig.java
+++ src/br/com/cwmconsultoria/bd/DBConfig.java
@@ -8,7 +8,7 @@ public class DBConfig {
 
 	public static Connection getConnection() throws SQLException, ClassNotFoundException{
 		Class.forName("com.mysql.jdbc.Driver");
-		return DriverManager.getConnection("jdbc:mysql://192.168.15.10:3306/financas_db", "root", "cwmRoot15") ;
+		return DriverManager.getConnection("jdbc:mysql://192.168.15.15:3306/financas_db", "root", "cwmRoot15") ;
 		//return DriverManager.getConnection("jdbc:mysql://cwmnote:3306/financas_db", "root", "cwmRoot15") ;
 	}
 }
